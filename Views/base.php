a16bf6a929a53fba57d16488b40cc1468f09102a
UnSeulChemin <bendev4977@gmail.com>
2026-04-10 20:12:05 +0200

    update

--- Views/base.php
+++ Views/base.php
@@ -5,15 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title; ?></title>
-    <link rel="shortcut icon" type="image/png" href="<?= $basePath; ?>public/images/favicon/favicon.png">
-    <link rel="stylesheet" type="text/css" href="<?= $basePath; ?>public/css/reset.css">
-    <link rel="stylesheet" type="text/css" href="<?= $basePath; ?>public/css/app.css">
+    <link rel="shortcut icon" href="<?= $basePath; ?>public/images/favicon/favicon.png">
+    <link rel="stylesheet" href="<?= $basePath; ?>public/css/app.css">
     <script src="https://kit.fontawesome.com/118716b668.js" crossorigin="anonymous"></script>
 </head>
-<body>
-
-<?php require_once 'partials/header.php'; ?>
-<main><?= $content ?></main>
-
-</body>
-</html>
