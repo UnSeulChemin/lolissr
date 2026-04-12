c8fcfc283b55729e3750103b1ed1f2f861e9184d
UnSeulChemin <bendev4977@gmail.com>
2026-04-13 00:26:43 +0200

    update

--- Views/partials/header.php
+++ Views/partials/header.php
@@ -8,8 +8,19 @@ $activeManga = str_contains($currentPage, 'manga') ? 'active' : '';
     <nav class="flex-center-center">
 
         <ul class="flex-gap-50">
-            <li><a class="<?= $activeHome ?> link-menu" href="<?= $basePath; ?>">Accueil</a></li>
-            <li><a class="<?= $activeManga ?> link-menu" href="<?= $basePath; ?>manga">Manga</a></li>
+            <li>
+                <a class="<?= $activeHome ?> link-menu"
+                   href="<?= $basePath; ?>">
+                   Accueil
+                </a>
+            </li>
+
+            <li>
+                <a class="<?= $activeManga ?> link-menu"
+                   href="<?= $basePath; ?>manga">
+                   Manga
+                </a>
+            </li>
         </ul>
 
     </nav>
