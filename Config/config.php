dac5ba5f1b47bfb24c055d550689e310d4c30ac4
UnSeulChemin <bendev4977@gmail.com>
2026-04-12 04:30:03 +0200

    update

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Configuration globale du projet
+ */
+
 return [
+
+    /**
+     * chemin de base du projet
+     * utile pour générer les URLs
+     * ex: http://localhost/lolissr/
+     */
     'base_path' => '/lolissr/',
+
 ];
--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -5,10 +5,25 @@ use App\Core\Functions;
 
 abstract class Controller
 {
+    /**
+     * template principal utilisé (base.php)
+     */
     protected string $template = 'base';
+
+    /**
+     * titre par défaut
+     */
     protected string $title = 'LoliSSR';
+
+    /**
+     * chemin de base du site (/lolissr/)
+     */
     protected string $basePath;
 
+    /**
+     * constructeur
+     * initialise le basePath depuis la config
+     */
     public function __construct()
     {
         $this->basePath = Functions::basePath();
@@ -16,16 +31,35 @@ abstract class Controller
 
     public function render(string $file, array $data = []): void
     {
-        extract($data);
+        extract($data, EXTR_SKIP);
 
         $title = $this->title;
         $basePath = $this->basePath;
 
+        /* chemin vue */
+        $viewPath = ROOT . '/Views/' . $file . '.php';
+
+        if (!file_exists($viewPath))
+        {
+            http_response_code(404);
+            exit('Vue introuvable : ' . $file);
+        }
+
         ob_start();
-        require_once(ROOT . '/Views/' . $file . '.php');
+
+        require_once $viewPath;
 
         $content = ob_get_clean();
 
-        require_once(ROOT . '/Views/' . $this->template . '.php');
+        /* chemin template */
+        $templatePath = ROOT . '/Views/' . $this->template . '.php';
+
+        if (!file_exists($templatePath))
+        {
+            http_response_code(500);
+            exit('Template introuvable : ' . $this->template);
+        }
+
+        require_once $templatePath;
     }
 }
