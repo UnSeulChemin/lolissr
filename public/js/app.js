355df4ef05957da5f5f2286395e560111463d250
UnSeulChemin <bendev4977@gmail.com>
2026-04-15 16:49:52 +0200

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,14 @@
 import { initPaginationAjax } from './pagination.js';
 import { initAjaxNotes } from './notes.js';
 import { initAutoSlug } from './slug.js';
+import { showToast } from './toast.js';
 
 document.addEventListener('DOMContentLoaded', () =>
 {
     initPaginationAjax();
     initAjaxNotes();
     initAutoSlug();
+
+    // Rend showToast accessible globalement
+    window.showToast = showToast;
 });
