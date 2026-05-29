9cac7df0c13d4178807b3a576ea4b6dcac29c9e0
UnSeulChemin <bendev4977@gmail.com>
2026-05-29 10:52:39 +0200

    refrato

---
+++ tests/Http/cases/safe/routes-canonical.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+$tests[] = [
+
+    'category' => 'Canonical',
+
+    'label' => 'Slug manga non canonique',
+
+    'path' => '/manga/series/I-WANT-TO-SEE-YOU-SHY/1',
+
+    'expected_status' => 301,
+];
