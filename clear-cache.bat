7bc9b71b9415284b620ac01046823480aedcd27b
UnSeulChemin <bendev4977@gmail.com>
2026-06-07 03:08:01 +0200

    refrato

---
+++ clear-cache.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo.
+echo ==========================
+echo      CLEAR CACHE
+echo ==========================
+echo.
+
+for %%f in (storage\cache\*) do (
+    if /I not "%%~nxf"==".gitkeep" (
+        del /f /q "%%f"
+    )
+)
+
+echo.
+echo Cache vide.
+echo.
+
+pause
