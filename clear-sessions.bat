231f5348b264893beb3b9f1c3d5e50de57ad51b3
UnSeulChemin <bendev4977@gmail.com>
2026-06-10 05:04:18 +0200

    refrato

---
+++ clear-sessions.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo.
+echo ==========================
+echo     CLEAR SESSIONS
+echo ==========================
+echo.
+
+for %%f in (storage\sessions\*) do (
+    if /I not "%%~nxf"==".gitkeep" (
+        del /f /q "%%f"
+    )
+)
+
+echo.
+echo Sessions videes.
+echo.
+
+pause
