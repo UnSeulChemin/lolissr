93dc85bd17b4c5e35be925e87a2d91569153e8f0
UnSeulChemin <bendev4977@gmail.com>
2026-06-14 01:47:56 +0200

    polish

--- Config/app.php
+++ Config/app.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 return [
     'name' => env('APP_NAME', 'Site'),
-
+    'version' => env('APP_VERSION', '1.0.0'),
     'base_uri' => env('APP_BASE_URI', '/lolissr'),
-
     'env' => env('APP_ENV', 'local'),
-
     'debug' => env_bool('APP_DEBUG', false),
-
     'pagination' => max(1, env_int('APP_PAGINATION', 8)),
 ];
