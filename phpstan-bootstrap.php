7ebc0ea22b8e2c6b25271e669e27fc9a348a67a6
UnSeulChemin <bendev4977@gmail.com>
2026-06-23 15:05:56 +0200

    polish

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-if (!defined('ROOT'))
+if (! defined('ROOT'))
 {
     define('ROOT', __DIR__);
 }
 
-require_once ROOT . '/vendor/autoload.php';
-require_once ROOT . '/Framework/Support/helpers.php';
+require ROOT . '/vendor/autoload.php';
+require ROOT . '/Framework/Support/helpers.php';
